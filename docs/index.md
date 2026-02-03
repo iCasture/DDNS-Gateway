6b1f1b9b382823fdf494ec6aba897232720dde7e
iCasture <iCasture@gmail.com>
2026-02-03 18:13:58 +0800

    docs: set up Sphinx documentation

---
+++ docs/index.md
@@ -0,0 +1,15 @@
+```{include} ../README.md
+```
+
+## API Reference
+
+完整的 API 文档请参阅 [API Reference](autoapi/ddns_gateway/index)。
+
+```{toctree}
+:maxdepth: 3
+:hidden:
+<!-- :caption: API Reference -->
+
+autoapi/ddns_gateway/index
+<!-- API Reference <autoapi/ddns_gateway/index> -->
+```
--- src/ddns_gateway/__init__.py
+++ src/ddns_gateway/__init__.py
@@ -2,8 +2,8 @@
 DDNS Gateway - A DDNS update service for RouterOS.
 
 This package provides a gateway service that bridges RouterOS scripts
-with DNS providers (CloudFlare, Alibaba Cloud DNS, Tencent Cloud DNSPod).
+with DNS providers (Cloudflare, Alibaba Cloud DNS, Tencent Cloud DNSPod).
 """
 
 __version__ = "0.1.0"
-__author__ = "DDNS Gateway Contributors"
+__author__ = "iCasture"
